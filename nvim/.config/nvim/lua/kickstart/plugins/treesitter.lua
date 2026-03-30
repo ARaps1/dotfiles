ffd332b470505eedfaeada33794b2c90499cd6c0
Allan Raposo <allan.raposo@benchling.com>
2026-03-30 11:14:26 -0400

    fixed treesitter

--- nvim/.config/nvim/lua/kickstart/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/treesitter.lua
@@ -9,7 +9,7 @@ return {
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter-intro`
     config = function()
       local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'python', 'query', 'tsx', 'typescript', 'vim', 'vimdoc' }
-      require('nvim-treesitter').install(parsers)
+      require('nvim-treesitter').install(parsers, { prefer_git = true })
       vim.api.nvim_create_autocmd('FileType', {
         callback = function(args)
           local buf, filetype = args.buf, args.match
